753506392b3b758292b111bff1dfc33c8a5beb30
KIM HEONIL <iamheonil@gmail.com>
2021-09-08 21:31:28 +0900

    메인 매핑 URL 수정, 혹시나 index.html 페이지로 이동되면 '/' 로 이동할 수 있게 스크립트 수정

--- src/main/java/com/heonil/springboot/controller/mainController.java
+++ src/main/java/com/heonil/springboot/controller/mainController.java
@@ -6,7 +6,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 @Controller
 public class mainController {
 
-    @RequestMapping(value = "/main")
+    @RequestMapping(value = "/")
     public String main() {
         return "main";
     }
